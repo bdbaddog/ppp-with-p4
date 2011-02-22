5109e1ddaf1a21023761e15349c555329187795a
William Deegan <bill@baddogconsulting.com>
2011-02-22 09:56:26 -0800

    chapter 7, not really working, looks like Install.py and setuphandlers.py are not being run

---
+++ src/optilux.policy/optilux/policy/setuphandlers.py
@@ -0,0 +1,32 @@
+from Products.CMFCore.utils import getToolByName
+
+def setupGroups(portal):
+    acl_users=getToolByName(portal,'acl_users')
+    if not acl_users.searchGroups(name='Staff'):
+        gtool=getToolByName(portal,'portal_groups')
+        gtool.addGroup('Staff',roles=['StaffMember'])
+
+
+def renameRichDocument(portal):
+    portal_types = getToolByName(portal,'portal_types')
+    rich_document_fti = getattr(portal_types,'RichDocument')
+    rich_document_fti.title = "Web Page"
+
+def disableDocument(portal):
+    portal_types = getToolByName(portal,'portal_types')
+    document_fti = getattr(portal_types,'Document')
+    document_fti.global_allow = False
+
+
+
+def importVarious(context):
+    """Miscellaneous steps import handler
+    """
+#    if context.readDataFile('optilux.policy_various.txt') is None:
+#        return
+    import pdb; pdb.set_trace()
+
+    portal = context.getSite()
+    setupGroups(portal)
+    renameRichDocument(portal)
+    disableDocument(portal)
