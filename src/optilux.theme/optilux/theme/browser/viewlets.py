b2a07a1668e22bbcc74c39ccebdc3868a2bbfa08
William Deegan <bill@baddogconsulting.com>
2011-02-23 15:05:38 -0800

    mostly functional chapter 8. the theming is still not showing the column-one stuff, may need to recopy some of the login_*.pt's into  src/optilux.theme/optilux/theme/skins/optilux_theme_custom_templates/

--- src/optilux.theme/optilux/theme/browser/viewlets.py
+++ src/optilux.theme/optilux/theme/browser/viewlets.py
@@ -1,21 +1,14 @@
+from zope.component import getMultiAdapter
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import ViewletBase
+from plone.app.layout.viewlets import common
 
-# Sample code for a basic viewlet (In order to use it, you'll have to):
-# - Un-comment the following useable piece of code (viewlet python class).
-# - Rename the viewlet template file ('browser/viewlet.pt') and edit the
-#   following python code accordingly.
-# - Edit the class and template to make them suit your needs.
-# - Make sure your viewlet is correctly registered in 'browser/configure.zcml'.
-# - If you need it to appear in a specific order inside its viewlet manager,
-#   edit 'profiles/default/viewlets.xml' accordingly.
-# - Restart Zope.
-# - If you edited any file in 'profiles/default/', reinstall your package.
-# - Once you're happy with your viewlet implementation, remove any related
-#   (unwanted) inline documentation  ;-p
+class PathBarViewlet(common.PathBarViewlet):
+    """A custom version of the path bar (breadcrumbs) viewlet, which 
+    uses slightly different markup.
+    """
+    
+    render = ViewPageTemplateFile('templates/path_bar.pt')
+    
+    # The update() method, inherited from the base class, takes care of
+    # initializing various variables used in the template
 
-#class MyViewlet(ViewletBase):
-#    render = ViewPageTemplateFile('viewlet.pt')
-#
-#    def update(self):
-#        self.computed_value = 'any output'
